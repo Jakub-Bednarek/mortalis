6ca7dfa5f3b5da3c22459482389986c8803f1ebf
Jakub-Bednarek <jakub.bednare1009@gmail.com>
2025-01-28 00:35:18 +0100

    Fixed format script to find *.h files instead of *.hpp

--- format.sh
+++ format.sh
@@ -2,6 +2,6 @@
 
 MORTALIS_SOURCE_DIR=$(pwd)/Source/Mortalis
 
-FILES=$(find $MORTALIS_SOURCE_DIR -type f -regex ".*\.[hpp|cpp]")
+FILES=$(find $MORTALIS_SOURCE_DIR -type f -regex ".*\.[h|cpp]")
 
 for FILE in $FILES; do clang-format -i $FILE; done
